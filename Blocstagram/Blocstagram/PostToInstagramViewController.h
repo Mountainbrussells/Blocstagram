a0bf5964055b601a665c6c38a378f8f6e04134e6
Mountainbrussells <mountainbrussells@gmail.com>
2015-10-26 12:23:47 -0600

    Completed checkpoint 43

---
+++ Blocstagram/Blocstagram/PostToInstagramViewController.h
@@ -0,0 +1,17 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram
+//
+//  Created by Ben Russell on 10/26/15.
+//  Copyright © 2015 Ben Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype)initWithImage:(UIImage *)sourceImage;
+
+@end
